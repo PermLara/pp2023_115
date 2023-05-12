64c91ce89cd88f677025a6604dd5fecca16de72f
Larisa <lara_gil@mail.ru>
2023-05-12 09:44:07 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    final static String hostName = "localhost";
+    final static String dbName = "pp2023_114";
+    final static String userName = "root";
+    final static String password = "1234";
+    final static String url = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+    public static Connection getMySQLConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, userName, password);
+            conn.setAutoCommit(false);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
 }
